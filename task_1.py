f45bcfd7cac5dd6ff05620047e1f8f3c849f4048
Svichkariov <svichkariov@yandex.by>
2022-12-10 16:10:12 +0300

    Добавлено задание 1. Свичкарев

--- task_1.py
+++ task_1.py
@@ -5,61 +5,126 @@
 
 from typing import Tuple
 
+# Смог решить:
 
 # Реализовать функции сложения, вычитания и умножения двух чисел.
 def add_mul(first: float, second: float) -> Tuple[float, float, float]:
-    return ..., ..., ...
-
+    return first + second, first - second, first * second
 
 # Реализовать функции деления, деления нацело и нахождения остатка от деления.
 def div_int_rem(first: float, second: float) -> Tuple[float, float, float]:
-    return ..., ..., ...
-
+    return first / second, first // second, first % second
 
 # Обменять два целочисленных значение с помощью битового xor не используя промежуточноые переменные.
 def xor_swap(first: int, second: int) -> int:
-    return ...
-
+    return (first ^ second)
 
 # Вернуть наименьшее число, используя условный оператор.
 def min_conditional(first: float, second: float) -> float:
-    return ...
-
+    return first if first > second else second
 
 # Реализовать функции умножения на 2, 8 и 32 с помощью битовых операций сдвига.
 def mul_shift_2_8_32(value: int) -> Tuple[int, int, int]:
-    return ..., ..., ...
-
+    return value << 1, value << 3, value << 5
 
 # Реализовать функции деления на 2, 8 и 32 с помощью битовых операций сдвига.
 def div_shift_2_8_32(value: int) -> Tuple[int, int, int]:
-    return ..., ..., ...
-
+    return value >> 1, value >> 3, value >> 5
 
 # Реализовать операцию возведения в степень остатка от деления.
 def exponentiation(divident: float, divider: float, power: float) -> float:
-    return ...
-
-
-# Определить знак 32-битного числа с помощью операции &.
-def sign(value: int) -> int:
-    return ...
-
+    return (divident % divider)**power
 
 # Умножить целочисленное значение на -1 с помощью битовых операций и сложения.
 def change_sign(value: int) -> int:
-    return ...
-
+    return ~ value + 1
 
 # Возвратить True, если хотя бы один четный бит 32-х битного числа установлен в 1.
 def check_32_even_bit_set(value: int) -> bool:
-    return ...
+    j = 0
+    print(bin(value))
+    for i in range(0,32,2):
+        if value & (1 << i):
+            j += 1
+            break
+    return True if j > 0 else False
 
 
 # Посчитать количество бит в 32-х битном числе, установленных в ноль.
 def calculate_32_zero_bits(value: int) -> int:
-    return ...
+    print(bin(value))
+    j = len(bin(value))-1
+    for i in range(32):
+        if value & (1 << i):
+            j -= 1
+    return j
 
+# Ограничить число заданным интервалом. Нижняя граница заданного интервала меньше либо равна верхней.
+def clamp(value: float, low: float, high: float) -> float:
+    if value <= low:
+        return low
+    elif value <= high:
+        return value
+    else:
+        return high
+
+# Вернуть True, если число нечетно и входит в интервал от -10 до 10.
+def event_and_match_interval_m10_10(value: int) -> bool:
+    return value // 2 and -10 < value < 10
+
+
+# Определить порядок выолнения операций и расстваить скобки таким образом, чтобы он стал обратным.
+def reverse_operations(value: float):
+    return (value ** (4 * (0.5 + 0.25)))
+
+
+# Установить n-ый бит числа в единицу.
+def set_nth_bit(value: int, n: int) -> int:
+    return value | (1<<n)
+
+# Расставить скобки таким образом, чтобы выражение в задаче было возведено в степень 3.
+def change_priorities(x: float) -> float:
+    return x + 3 ** 3
+# мое решение
+def change_priorities(x: float) -> float:
+    return (x + 3) ** 3
+
+
+'''
+Задачи повышенной сложности.
+'''
+# Вернуть наименьшее целое число без использования условных операторов и встроенных функций.
+def min_raw(first: int, last: int) -> int:
+    return ...
+# мое решение
+def min_raw(first: int, last: int) :
+    k = 0
+    while first > last:
+        k += 1
+        return last
+        k = 1
+        break
+    else:
+        return first
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Не смог решить:
+
+# Определить знак 32-битного числа с помощью операции &.
+def sign(value: int) -> int:
+    return ...
 
 # Упаковать два целочисленных значения в 8 бит.
 # Первое число должно располагаться в 4 младших битах, второе число в - 4 старших.
@@ -72,40 +137,20 @@ def pack_4_4(first: int, second: int) -> int:
 def unpack_4_4(first: int, second: int) -> int:
     return ...
 
-
-# Ограничить число заданным интервалом. Нижняя граница заданного интервала меньше либо равна верхней.
-def clamp(value: float, low: float, high: float) -> float:
-    return ...
-
-
 # Ограничить число заданным интервалом. Нижняя граница может быть как меньше, так и больше верхней.
 def clamp_any(value: float, low: float, high: float) -> float:
     return ...
-
-
-# Вернуть True, если число нечетно и входит в интервал от -10 до 10.
-def event_and_match_interval_m10_10(value: int) -> bool:
-    return ...
-
-
-# Определить порядок выолнения операций и расстваить скобки таким образом, чтобы он стал обратным.
-def reverse_operations(value: float):
-    return value ** 4 * 0.5 + 0.25
-
-
-# Установить n-ый бит числа в единицу.
-def set_nth_bit(value: int, n: int) -> int:
-    return ...
-
 
 # Переключить n-ый бит числа.
 def switch_nth_bit(value: int, n: int) -> int:
     return ...
 
-
 # Расставить скобки таким образом, чтобы выражение в задаче было возведено в степень 3.
 def change_priorities(x: float) -> float:
     return x + 3 ** 3
+# мое решение
+def change_priorities(x: float) -> float:
+    return (x + 3) ** 3
 
 
 '''
@@ -120,9 +165,4 @@ def change_priorities(x: float) -> float:
 # https://habr.com/ru/post/337260/
 # Для решения этой задачи нужно также выяснить, как конвертировать байты в целые и дробные числа.
 def int_to_float(value: int) -> float:
-    return ...
-
-
-# Вернуть наименьшее целое число без использования условных операторов и встроенных функций.
-def min_raw(first: int, last: int) -> int:
     return ...
