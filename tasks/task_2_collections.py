5b192d8e639e941daaabb10b95c790b115523b22
Sasha K <a.kosak.mbx@gmail.com>
2022-12-14 17:48:46 +0300

    Добавлено задание 2. Коллекции

---
+++ tasks/task_2_collections.py
@@ -0,0 +1,241 @@
+'''
+Задание 2.
+Коллекции.
+Примечание: входные параметры ни в однй из задач не должны быть модифицированы.
+'''
+
+from typing import Any, Dict, Iterable, List, Tuple
+
+
+# Сконструировать и вернуть список из переданных аргументов.
+def build_list_from_args(*args) -> List:
+    ...
+
+
+# Сконструировать и вернуть список из переданных аргументов, имеющих тип int.
+def build_int_list_from_args(*args) -> List[int]:
+    ...
+
+
+# Сконструировать и вернуть список из переданных аргументов, имеющих заданный тип.
+def build_list_from_args_using_type(argument_type: type, *args) -> List:
+    ...
+
+
+# Сконструировать и вернуть список из переданных аргументов, тип которых входит заданное множество.
+# Для более эффективной работы преобразовать `argument_types` в `set`.
+def build_list_from_args_using_type_set(argument_types: Iterable, *args) -> List:
+    ...
+
+
+# Сконструировать и вернуть список из двух списков, переданных в качестве аргументов.
+def build_list_from_two_lists(first: List, second: List) -> List:
+    ...
+
+
+# Сконструировать и вернуть список из неограниченного числа списков, переданных в качестве аргументов.
+def build_list_from_list_args(*lists) -> List:
+    ...
+
+
+# Сконструировать список из заданного элемента и значения длины (использовать умножение).
+def build_list_from_value_and_length(value: Any, length: int) -> List:
+    ...
+
+
+# Удалить из списка заданный элемент.
+def remove_value_from_list(values: List, value_to_remove: Any) -> List:
+    ...
+
+
+# Удалить из списка заданный элемент, используя comprehension expression [... for .. in ...].
+def remove_value_from_list_using_comprehension(values: List, value_to_remove: Any) -> List:
+    ...
+
+
+# Удалить из списка заданный элемент, используя `filter` и lambda-функцию.
+def remove_value_from_list_using_filter(values: List, value_to_remove: Any) -> List:
+    ...
+
+
+# Удалить из списка заданные элементы. Преобразовать `values_to_remove` в `set`.
+def remove_values_from_list(values: List, values_to_remove: Iterable) -> List:
+    ...
+
+
+# Удалить из списка заданные элементы. Преобразовать `values_to_remove` в `set`.
+def remove_values_from_list_using_comprehension(values: List, value_to_remove: Any) -> List:
+    ...
+
+
+# Удалить из списка заданные элементы. Преобразовать `values_to_remove` в `set`.
+# Использовать `filter` и lambda-функцию.
+def remove_values_from_list_using_filter(values: List, value_to_remove: Any) -> List:
+    ...
+
+
+# Удалить из списка дублирующиеся значения (использовать преобразование в `set` и обратно).
+def remove_duplicates_from_list(values: List) -> List:
+    ...
+
+
+# Создать и вернуть словарь из заданного набора именованных аргументов, значения которых имеют тип int.
+def build_dict_from_named_arguments_of_type_int(*kwargs) -> Dict:
+    ...
+
+
+# Создать и вернуть словарь, используя в качестве ключей аргумент функции,
+# а в качестве значений - None (dict.fromkeys).
+def build_dict_from_keys(values: Iterable) -> Dict:
+    ...
+
+
+# Создать и вернуть словарь, используя в качестве ключей аргумент функции,
+# а в качестве значений - None (dict.fromkeys).
+def build_dict_from_keys(values: Iterable) -> Dict:
+    ...
+
+
+# Создать и вернуть словарь, используя в качестве ключей аргумент функции,
+# а в качестве значений - значение по-умолчанию.
+def build_dict_from_keys_and_default(values: Iterable, default: Any) -> Dict:
+    ...
+
+
+# Создать и вернуть словарь, ключами которого являются индексы элементов,
+# а значениями - значения элементов iterable параметров (использовать enumerate и dict comprehension).
+def build_dict_from_indexed_values(values: Iterable) -> Dict:
+    ...
+
+
+# Создать и вернуть словарь, собранный на основе списка пар ключ-значение.
+def build_dict_from_key_value_pairs(kws: List[Tuple) -> Dict:
+    ...
+
+    # Создать и вернуть словарь, собранный из двух списков, один из которых
+    # содержит ключ, а второй - соответствующее значение (использовать zip).
+
+
+def build_dict_from_two_lists(keys: List, values: List) -> Dict:
+    ...
+
+
+# Сформировать из двух словарей и вернуть его. В случае, если ключи совпадают,
+# использовать значение из второго словаря (dict.update).
+def build_dict_using_update(first: Dict, second: Dict) -> Dict:
+    ...
+
+
+# Обновить словарь (и вернуть его), используя значения именованных аргументов.
+# Заменить значение в случае совпадения ключей.
+def update_dict_using_kwargs(dictionary: Dict, **kwargs) -> Dict:
+    return ...
+
+
+# Обновить словарь (и вернуть его), используя значения именованных аргументов.
+# Объединить значения в список в случае совпадения ключей.
+def update_and_merge_dict_using_kwargs(dictionary: Dict, **kwargs) -> Dict:
+    return ...
+
+
+# Объединить два словарь и вернуть результат.
+# Объединить значения в список в случае совпадения ключей.
+def merge_two_dicts(first: Dict, second: Dict) -> Dict:
+    return ...
+
+
+# Объединить два словарь и вернуть результат.
+# В случае совпадения ключей:
+# - объединить значения рекурсивно, если оба значения - словари;
+# - объединить значения в один список, если оба значения - списки;
+# - объединить значения в одно множество, если оба значения - множества;
+# - объединить значения в список в любом другом случае.
+def deep_merge_two_dicts(first: Dict, second: Dict) -> Dict:
+    return ...
+
+
+# Вернуть список, состоящий из ключей, принадлежащих словарю.
+def get_keys(dictionary: Dict) -> List:
+    ...
+
+
+# Вернуть список, состоящий из значений, принадлежащих словарю.
+def get_values(dictionary: Dict) -> List:
+    ...
+
+
+# Вернуть список, состоящий из пар ключ-значение, принадлежащих словарю.
+def get_key_value_pairs(dictionary: Dict) -> List[Tuple]:
+    ...
+
+
+# Реверсировать и вернуть словарь.
+def reverse_dict(dictionary: Dict) -> Dict:
+    ...
+
+
+# Удалить из словаря элементы, имеющие пустые значения (None, '', [], {}).
+def clear_dummy_elements(dictionary: Dict) -> Dict:
+    ...
+
+
+# Удалить из словаря дублирующиеся и пустые элементы.
+def clear_dummy_and_duplicate_elements(dictionary: Dict) -> Dict:
+    ...
+
+
+# Обменять в словаре клчи и значения (в качестве значений могут выступать только неизменяемые значения).
+def swap_dict_keys_and_values(dictionary: Dict) -> Dict:
+    ...
+
+
+# Вернуть словарь, отсортированный по ключу. Ключи могут иметь только тип int.
+def sort_dict_with_int_keys(dictionary: Dict) -> Dict:
+    ...
+
+
+# Вернуть словарь, отсортированный по ключу в обратном порядке. Ключи могут иметь только тип int.
+def sort_dict_backward_with_int_keys(dictionary: Dict) -> Dict:
+    ...
+
+
+# Вернуть словарь, элементы которого сгруппированы по типу ключа.
+# В качестве ключей могут выступать: целые числа, дробные числа и строки.
+# Приоритет сортировки групп (от высшего к низшему): целые числа, дробные числа, строки.
+def group_dict_elements_by_key_type(dictionary: Dict) -> Dict:
+    ...
+
+
+# Вернуть словарь, элементы которого сгруппированы по типу ключа.
+# В качестве ключей могут выступать: целые числа, дробные числа и строки.
+# Приоритет сортировки групп (от высшего к низшему): целые числа, дробные числа, строки.
+# Внутри каждой из групп отсортировать элементы по значениям ключа в обратном порядке.
+def group_dict_elements_by_key_type_and_sort(dictionary: Dict) -> Dict:
+    ...
+
+
+# Подсчитать количество элементов словаря, имеющих числовой тип, значение которых находится
+# в интервале [-10, 25].
+def count_dict_elements(dictionary: Dict) -> Dict:
+    ...
+
+
+# Построить и возвратить словарь из двух списков. Количество ключей может превышать
+# количество значений. В этом случае (для ключей, оставшихся без соответствующей пары)
+# в качестве значений использовать значение None.
+def build_dict_from_two_unaligned_lists(keys: List, values: List) -> Dict:
+    ...
+
+
+# Построить и возвратить словарь из двух списков. Количество ключей может превышать
+# количество значений. В этом случае (для ключей, оставшихся без соответствующей пары)
+# в качестве значений использовать значение, заданное по-умолчанию.
+def build_dict_from_two_unaligned_lists_and_default(keys: List, values: List, default: Any) -> Dict:
+    ...
+
+
+# Построить и возвратить словарь из двух iterable объектов. Количество ключей может превышать
+# количество значений. В этом случае (для ключей, оставшихся без соответствующей пары)
+# в качестве значений использовать значение None.
+def build_dict_from_two_unaligned_iterables(keys: Iterable, values: Iterable) -> Dict:
+    ...
