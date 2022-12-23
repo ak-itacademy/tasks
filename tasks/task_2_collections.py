067a57a620de3a8e6fdfe5b59cea8585f72730ff
Melklo <86657154+Melklo@users.noreply.github.com>
2022-12-23 20:16:30 +0300

    Add files via upload

--- tasks/task_2_collections.py
+++ tasks/task_2_collections.py
@@ -9,33 +9,63 @@ from typing import Any, Dict, Iterable, List, Tuple
 
 # Сконструировать и вернуть список из переданных аргументов.
 def build_list_from_args(*args) -> List:
-    ...
+    return list(args)
+
+
+print(build_list_from_args(3, '1', 23.5))
 
 
 # Сконструировать и вернуть список из переданных аргументов, имеющих тип int.
 def build_int_list_from_args(*args) -> List[int]:
-    ...
+    answer = args.formkeys(['int', 'str', 'float', 'dict'])
+    c = []
+    for i in args:
+        if type(args[i]) == int:
+            c += ([i, args[i]])
+            answer['int'] = c
+    return list(answer)
+
+
+print(build_list_from_args(1, 2, 3, 'str', 1.5))
 
 
 # Сконструировать и вернуть список из переданных аргументов, имеющих заданный тип.
 def build_list_from_args_using_type(argument_type: type, *args) -> List:
-    ...
+    li = []
+    for i in args:
+        if type(args[i]) == argument_type:
+            li += ([i, args[i]])
+        return li
+    print('error')
 
 
 # Сконструировать и вернуть список из переданных аргументов, тип которых входит заданное множество.
 # Для более эффективной работы преобразовать `argument_types` в `set`.
 def build_list_from_args_using_type_set(argument_types: Iterable, *args) -> List:
-    ...
+    li = []
+    for i in args:
+        if type(args) == set(argument_types):
+            li += args[i]
+    return args
+
+
+# print(build_list_from_args_using_type_set(a=34, b='some text', c=2, d=1.3, e={1: 2}, f=-3.0))
 
 
 # Сконструировать и вернуть список из двух списков, переданных в качестве аргументов.
 def build_list_from_two_lists(first: List, second: List) -> List:
-    ...
+    return first + second
+
+
+print(build_list_from_two_lists([1, 2.5, 3], [4, 5, 6]))
 
 
 # Сконструировать и вернуть список из неограниченного числа списков, переданных в качестве аргументов.
 def build_list_from_list_args(*lists) -> List:
-    ...
+    return [j for i in lists for j in i]
+
+
+print(build_list_from_list_args([1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 'str']))
 
 
 # Сконструировать список из заданного элемента и значения длины (использовать умножение).
@@ -45,22 +75,25 @@ def build_list_from_value_and_length(value: Any, length: int) -> List:
 
 # Удалить из списка заданный элемент.
 def remove_value_from_list(values: List, value_to_remove: Any) -> List:
-    ...
+    return values.remove(value_to_remove)
 
 
 # Удалить из списка заданный элемент, используя comprehension expression [... for .. in ...].
 def remove_value_from_list_using_comprehension(values: List, value_to_remove: Any) -> List:
-    ...
+    return [value_to_remove for i in values.remove(value_to_remove) for value_to_remove in i]
 
 
 # Удалить из списка заданный элемент, используя `filter` и lambda-функцию.
 def remove_value_from_list_using_filter(values: List, value_to_remove: Any) -> List:
-    ...
+    c = list(filter(lambda x: values.remove(value_to_remove)))
+    return c
 
 
 # Удалить из списка заданные элементы. Преобразовать `values_to_remove` в `set`.
 def remove_values_from_list(values: List, values_to_remove: Iterable) -> List:
-    ...
+    for i in values:
+        i += set(values_to_remove)
+    return values
 
 
 # Удалить из списка заданные элементы. Преобразовать `values_to_remove` в `set`.
@@ -71,35 +104,47 @@ def remove_values_from_list_using_comprehension(values: List, value_to_remove: A
 # Удалить из списка заданные элементы. Преобразовать `values_to_remove` в `set`.
 # Использовать `filter` и lambda-функцию.
 def remove_values_from_list_using_filter(values: List, value_to_remove: Any) -> List:
-    ...
+    for i in values:
+        i += set(value_to_remove)
+    return values
 
 
 # Удалить из списка дублирующиеся значения (использовать преобразование в `set` и обратно).
 def remove_duplicates_from_list(values: List) -> List:
-    ...
+    set(values)
+    return values
 
 
 # Создать и вернуть словарь из заданного набора именованных аргументов, значения которых имеют тип int.
-def build_dict_from_named_arguments_of_type_int(*kwargs) -> Dict:
-    ...
+def build_dict_from_named_arguments_of_type_int(**kwargs) -> Dict:
+    answer = kwargs.fromkeys(['int', 'str', 'float', 'dict'])
+    i_n = []
+    for i in kwargs:
+        if type(kwargs[i]) == int:
+            i_n += ([i, kwargs[i]],)
+            answer['int'] = i_n
+    return answer
 
 
 # Создать и вернуть словарь, используя в качестве ключей аргумент функции,
 # а в качестве значений - None (dict.fromkeys).
 def build_dict_from_keys(values: Iterable) -> Dict:
-    ...
+    c = dict.fromkeys(__iterable=values, __value=None)
+    return c
 
 
 # Создать и вернуть словарь, используя в качестве ключей аргумент функции,
 # а в качестве значений - None (dict.fromkeys).
 def build_dict_from_keys(values: Iterable) -> Dict:
-    ...
+    c = dict.fromkeys(__iterable=values, __value=None)
+    return c
 
 
 # Создать и вернуть словарь, используя в качестве ключей аргумент функции,
 # а в качестве значений - значение по-умолчанию.
 def build_dict_from_keys_and_default(values: Iterable, default: Any) -> Dict:
-    ...
+    c = dict.fromkeys(__iterable=values, __value=default)
+    return c
 
 
 # Создать и вернуть словарь, ключами которого являются индексы элементов,
@@ -109,39 +154,49 @@ def build_dict_from_indexed_values(values: Iterable) -> Dict:
 
 
 # Создать и вернуть словарь, собранный на основе списка пар ключ-значение.
-def build_dict_from_key_value_pairs(kws: List[Tuple) -> Dict:
-    ...
+def build_dict_from_key_value_pairs(kws: List[Tuple]) -> Dict:
+    return dict(kws)
 
     # Создать и вернуть словарь, собранный из двух списков, один из которых
     # содержит ключ, а второй - соответствующее значение (использовать zip).
 
 
 def build_dict_from_two_lists(keys: List, values: List) -> Dict:
-    ...
+    dict(keys)
+    dict(values)
+    return dict(zip(keys, values))
 
 
 # Сформировать из двух словарей и вернуть его. В случае, если ключи совпадают,
 # использовать значение из второго словаря (dict.update).
 def build_dict_using_update(first: Dict, second: Dict) -> Dict:
-    ...
+    if first == second:
+        return first.update(second)
+    return first + second
 
 
 # Обновить словарь (и вернуть его), используя значения именованных аргументов.
 # Заменить значение в случае совпадения ключей.
 def update_dict_using_kwargs(dictionary: Dict, **kwargs) -> Dict:
-    return ...
+    if dictionary.keys() == kwargs:
+        return dictionary.update(kwargs)
+    return dictionary
 
 
 # Обновить словарь (и вернуть его), используя значения именованных аргументов.
 # Объединить значения в список в случае совпадения ключей.
 def update_and_merge_dict_using_kwargs(dictionary: Dict, **kwargs) -> Dict:
-    return ...
+    if dictionary.keys() == kwargs:
+        return list(dictionary.update(kwargs))
+    return dictionary
 
 
 # Объединить два словарь и вернуть результат.
 # Объединить значения в список в случае совпадения ключей.
 def merge_two_dicts(first: Dict, second: Dict) -> Dict:
-    return ...
+    if first.keys() == second.keys():
+        return list(first + second)
+    return first + second
 
 
 # Объединить два словарь и вернуть результат.
@@ -156,32 +211,32 @@ def deep_merge_two_dicts(first: Dict, second: Dict) -> Dict:
 
 # Вернуть список, состоящий из ключей, принадлежащих словарю.
 def get_keys(dictionary: Dict) -> List:
-    ...
+    return list(dictionary.keys())
 
 
 # Вернуть список, состоящий из значений, принадлежащих словарю.
 def get_values(dictionary: Dict) -> List:
-    ...
+    return list(dictionary.items())
 
 
 # Вернуть список, состоящий из пар ключ-значение, принадлежащих словарю.
 def get_key_value_pairs(dictionary: Dict) -> List[Tuple]:
-    ...
+    return list(dictionary)
 
 
 # Реверсировать и вернуть словарь.
 def reverse_dict(dictionary: Dict) -> Dict:
-    ...
+    return reversed(dictionary)
 
 
 # Удалить из словаря элементы, имеющие пустые значения (None, '', [], {}).
 def clear_dummy_elements(dictionary: Dict) -> Dict:
-    ...
+    return dict(filter(lambda x: x[1], dictionary.items()))
 
 
-# Удалить из словаря дублирующиеся и пустые элементы.
+# Удалить из словаря дублирующиеся и пустые элементы.?
 def clear_dummy_and_duplicate_elements(dictionary: Dict) -> Dict:
-    ...
+    return dict(filter(lambda x: x[1], dictionary.items() and dictionary.items()))
 
 
 # Обменять в словаре клчи и значения (в качестве значений могут выступать только неизменяемые значения).
