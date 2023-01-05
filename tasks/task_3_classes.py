c8032f194d55b56ae56e2a0d861305bbbd3a5423
Evgeni121 <darevblago121@mail.ru>
2023-01-05 22:18:06 +0300

    task_3_classes v.02

--- tasks/task_3_classes.py
+++ tasks/task_3_classes.py
@@ -3,6 +3,8 @@
 Классы. Наследование, волшебные методы.
 '''
 
+from typing import Union
+
 # Необходимо реализовать семейство классов, обеспечивающих прозрачную работу с такими единицами
 # измерения, как миллиметры, сантиметры, метры, километры, дюймы, футы, ярды, фэнь, чи и инь.
 
@@ -33,35 +35,35 @@
 class LengthUnits:
     UNIT_NAME: str = "Length Units"
 
-    def __init__(self, value: float or "LengthUnits", conversion_rate: float):
+    def __init__(self, value: Union[float, "LengthUnits"], conversion_rate: float):
         self.__conversion_rate = float(conversion_rate)
-        self.__value = float(value if isinstance(value, float) or isinstance(value, int) else
-                             value.value() * value.conversion_rate() / self.conversion_rate())
+        self.__value = float(value) * self.__conversion_rate if isinstance(value, float) or isinstance(value, int) \
+            else value.value()
 
     def __eq__(self, another: "LengthUnits"):
-        return self.value() * self.conversion_rate() == another.conversion_to_base_unit()
+        return self.value() == another.value()
 
     def __lt__(self, another: "LengthUnits"):
-        return self.value() * self.conversion_rate() < another.conversion_to_base_unit()
+        return self.value() < another.value()
 
     def __add__(self, another):
-        self.__value += (another if isinstance(another, float) or isinstance(another, int) else
-                         another.conversion_to_base_unit() / self.conversion_rate())
+        self.__value += another * self.__conversion_rate if isinstance(another, float) or isinstance(another, int) \
+            else another.value()
         return self
 
     def __iadd__(self, another):
-        self.__value += (another if isinstance(another, float) or isinstance(another, int) else
-                         another.conversion_to_base_unit() / self.conversion_rate())
+        self.__value += another * self.__conversion_rate if isinstance(another, float) or isinstance(another, int) \
+            else another.value()
         return self
 
     def __sub__(self, another):
-        self.__value -= (another if isinstance(another, float) or isinstance(another, int) else
-                         another.conversion_to_base_unit() / self.conversion_rate())
+        self.__value -= another * self.__conversion_rate if isinstance(another, float) or isinstance(another, int) \
+            else another.value()
         return self
 
     def __isub__(self, another):
-        self.__value -= (another if isinstance(another, float) or isinstance(another, int) else
-                         another.conversion_to_base_unit() / self.conversion_rate())
+        self.__value -= another * self.__conversion_rate if isinstance(another, float) or isinstance(another, int) \
+            else another.value()
         return self
 
     def __mul__(self, value):
@@ -73,13 +75,13 @@ class LengthUnits:
         return self
 
     def __truediv__(self, another):
-        self.__value /= (another if isinstance(another, float) or isinstance(another, int) else
-                         another.conversion_to_base_unit() / self.conversion_rate())
+        self.__value /= another if isinstance(another, float) or isinstance(another, int) \
+            else another.value()
         return self if another is isinstance(another, float) or isinstance(another, int) else self.value()
 
     def __idiv__(self, another):
-        self.__value /= (another if isinstance(another, float) or isinstance(another, int) else
-                         another.conversion_to_base_unit() / self.conversion_rate())
+        self.__value /= another if isinstance(another, float) or isinstance(another, int)\
+            else another.value()
         return self if another is isinstance(another, float) or isinstance(another, int) else self.value()
 
     def value(self) -> float:
@@ -91,126 +93,134 @@ class LengthUnits:
     def conversion_rate(self) -> float:
         return self.__conversion_rate
 
-    def conversion_to_base_unit(self):
-        return self.value() * self.conversion_rate()
-
 
 class Millimeters(LengthUnits):
     UNIT_NAME: str = "Millimeters"
 
-    def __init__(self, value: float or "LengthUnits", conversion_rate=1):
+    def __init__(self, value: Union[float, "LengthUnits"], conversion_rate=1):
         super().__init__(value, conversion_rate)
 
     def __str__(self):
         value = self.value()
-        return f"{value} mm"
+        conversion_rate = self.conversion_rate()
+        return f"{value / conversion_rate} mm"
 
 
 class Centimeters(LengthUnits):
     UNIT_NAME: str = "Centimeters"
 
-    def __init__(self, value: float or "LengthUnits", conversion_rate=10):
+    def __init__(self, value: Union[float, "LengthUnits"], conversion_rate=10):
         super().__init__(value, conversion_rate)
 
     def __str__(self):
         value = self.value()
-        return f"{value} cm"
+        conversion_rate = self.conversion_rate()
+        return f"{value / conversion_rate} cm"
 
 
 class Meters(LengthUnits):
     UNIT_NAME: str = "Meters"
 
-    def __init__(self, value: float or "LengthUnits", conversion_rate=1000):
+    def __init__(self, value: Union[float, "LengthUnits"], conversion_rate=1000):
         super().__init__(value, conversion_rate)
 
     def __str__(self):
         value = self.value()
-        return f"{value} m"
+        conversion_rate = self.conversion_rate()
+        return f"{value / conversion_rate} m"
 
 
 class Kilometers(LengthUnits):
     UNIT_NAME: str = "Kilometers"
 
-    def __init__(self, value: float or "LengthUnits", conversion_rate=1000000):
+    def __init__(self, value: Union[float, "LengthUnits"], conversion_rate=1000000):
         super().__init__(value, conversion_rate)
 
     def __str__(self):
         value = self.value()
-        return f"{value} km"
+        conversion_rate = self.conversion_rate()
+        return f"{value / conversion_rate} km"
 
 
 class Inches(LengthUnits):
     UNIT_NAME: str = "Inches"
 
-    def __init__(self, value: float or "LengthUnits", conversion_rate=25.4):
+    def __init__(self, value: Union[float, "LengthUnits"], conversion_rate=25.4):
         super().__init__(value, conversion_rate)
 
     def __str__(self):
         value = self.value()
-        return f'{value} in'
+        conversion_rate = self.conversion_rate()
+        return f'{value / conversion_rate} in'
 
 
 class Feets(LengthUnits):
     UNIT_NAME: str = "Feets"
 
-    def __init__(self, value: float or "LengthUnits", conversion_rate=304.8):
+    def __init__(self, value: Union[float, "LengthUnits"], conversion_rate=304.8):
         super().__init__(value, conversion_rate)
 
     def __str__(self):
         value = self.value()
-        return f"{value} ft"
+        conversion_rate = self.conversion_rate()
+        return f"{value / conversion_rate} ft"
 
 
 class Yards(LengthUnits):
     UNIT_NAME: str = "Yards"
 
-    def __init__(self, value: float or "LengthUnits", conversion_rate=914.4):
+    def __init__(self, value: Union[float, "LengthUnits"], conversion_rate=914.4):
         super().__init__(value, conversion_rate)
 
     def __str__(self):
         value = self.value()
-        return f"{value} yd"
+        conversion_rate = self.conversion_rate()
+        return f"{value / conversion_rate} yd"
 
 
 class Miles(LengthUnits):
     UNIT_NAME: str = "Miles"
 
-    def __init__(self, value: float or "LengthUnits", conversion_rate=1_609_344):
+    def __init__(self, value: Union[float, "LengthUnits"], conversion_rate=1_609_344):
         super().__init__(value, conversion_rate)
 
     def __str__(self):
         value = self.value()
-        return f"{value} mi"
+        conversion_rate = self.conversion_rate()
+        return f"{value / conversion_rate} mi"
 
 
 class Fen(LengthUnits):
     UNIT_NAME: str = "Fen"
 
-    def __init__(self, value: float or "LengthUnits", conversion_rate=3.33):
+    def __init__(self, value: Union[float, "LengthUnits"], conversion_rate=3.33):
         super().__init__(value, conversion_rate)
 
     def __str__(self):
         value = self.value()
-        return f"{value} fen"
+        conversion_rate = self.conversion_rate()
+        return f"{value / conversion_rate} fen"
 
 
 class Chi(LengthUnits):
     UNIT_NAME: str = "Chi"
 
-    def __init__(self, value: float or "LengthUnits", conversion_rate=333):
+    def __init__(self, value: Union[float, "LengthUnits"], conversion_rate=333):
         super().__init__(value, conversion_rate)
 
     def __str__(self):
         value = self.value()
-        return f"{value} chi"
+        conversion_rate = self.conversion_rate()
+        return f"{value / conversion_rate} chi"
 
 
 class In(LengthUnits):
     UNIT_NAME: str = "In"
 
-    def __init__(self, value: float or "LengthUnits", conversion_rate=33_300):
+    def __init__(self, value: Union[float, "LengthUnits"], conversion_rate=33_300):
         super().__init__(value, conversion_rate)
 
     def __str__(self):
         value = self.value()
-        return f"{value} in"
+        conversion_rate = self.conversion_rate()
+        return f"{value / conversion_rate} in"
