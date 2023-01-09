90003dd6f9d1f2ef893a41fc662bb24995bf87e9
Evgeni121 <darevblago121@mail.ru>
2023-01-09 14:28:20 +0300

    task_2_functions v.04

--- tasks/task_2_functions.py
+++ tasks/task_2_functions.py
@@ -50,8 +50,7 @@ def type_func(*arg) -> Tuple:
 def grouping(**arg):
     my_dict = {}
     for key, value in arg.items():
-        my_type = type(value)
-        my_dict[my_type].append([key, value]) if my_type in my_dict else my_dict.setdefault(my_type, [[key, value]])
+        my_dict.setdefault(type(value), []).append([key, value])
     return my_dict
 
 
@@ -68,23 +67,18 @@ def grouping(**arg):
 # Вместо *name* должен быть подставлен именованный параметр с именем name.
 
 def function(text: str, *arg, **kwargs):
-    my_list = text.split()
+    my_list = text.split("*")
     for i in range(len(my_list)):
         line = my_list[i]
-        if "*" in line:
-            first_symbol = line.find("*")
-            second_symbol = line[first_symbol + 1:].find("*") + 2
-            element = line[first_symbol:second_symbol]
-
-            if not element[1:-1]:
-                my_list[i] = line.replace(element, "*")
-            elif element[1:-1].lstrip("-").isdigit():
-                my_list[i] = line.replace(element, str(arg[int(element[1:-1])]))
-            else:
-                my_list[i] = line.replace(element, str(kwargs[element[1:-1]]))
-    result = " ".join(my_list)
+        if line in kwargs:
+            my_list[i] = str(kwargs[line])
+        elif line.lstrip("-").isdigit():
+            my_list[i] = str(arg[int(line)])
+        elif not line:
+            my_list[i] = "*"
+    result = "".join(my_list)
     return result
 
 
-# print(function("This string contains elements ** kind of *2*, *-2*, *0*, *param_3*, *param_2*, *param_0*, *param_1*",
+# print(function("This string contains elements ** kind of *2*/*-2*/*0*, *2*, *1*, *0*, *param_3*, *param_2*, *param_0*, *param_1*.",
 #                10, "This", -29.5, param_0="Text", param_1=4, param_2=4.65, param_3=[1, 2, 3]))
