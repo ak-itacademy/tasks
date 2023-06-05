426e28552ac4734ea644fdb2316ecc01da7946bf
Stefan2501 <stef.12@mail.ru>
2023-06-05 12:38:08 +0300

    Task 1

--- tasks/task_1.py
+++ tasks/task_1.py
@@ -8,104 +8,113 @@ from typing import Tuple
 
 # Реализовать функции сложения, вычитания и умножения двух чисел.
 def add_mul(first: float, second: float) -> Tuple[float, float, float]:
-    return ..., ..., ...
+    return first + second, first - second, first * second
 
 
 # Реализовать функции деления, деления нацело и нахождения остатка от деления.
 def div_int_rem(first: float, second: float) -> Tuple[float, float, float]:
-    return ..., ..., ...
+    return first/second, first//second, first % second
 
 
 # Обменять два целочисленных значение с помощью битового xor не используя промежуточноые переменные.
-def xor_swap(first: int, second: int) -> int:
-    return ...
+def xor_swap(first: int, second: int) -> tuple[int, int]:
+    first ^= second
+    second ^= first
+    first ^= second
+    return first, second
 
 
 # Вернуть наименьшее число, используя условный оператор.
 def min_conditional(first: float, second: float) -> float:
-    return ...
+    return first if first < second else second
 
 
 # Реализовать функции умножения на 2, 8 и 32 с помощью битовых операций сдвига.
 def mul_shift_2_8_32(value: int) -> Tuple[int, int, int]:
-    return ..., ..., ...
+    return value << 1, value << 3, value << 5
 
 
 # Реализовать функции деления на 2, 8 и 32 с помощью битовых операций сдвига.
 def div_shift_2_8_32(value: int) -> Tuple[int, int, int]:
-    return ..., ..., ...
+    return value >> 1, value >> 3, value >> 5
 
 
 # Реализовать операцию возведения в степень остатка от деления.
 def exponentiation(divident: float, divider: float, power: float) -> float:
-    return ...
-
+    return (divident % divider)**power
 
 # Определить знак 32-битного числа с помощью операции &.
 def sign(value: int) -> int:
-    return ...
+    return  -1 if value >> 31 & 1 else 1
 
 
 # Умножить целочисленное значение на -1 с помощью битовых операций и сложения.
 def change_sign(value: int) -> int:
-    return ...
+    return ~value + 1
 
 
 # Возвратить True, если хотя бы один четный бит 32-х битного числа установлен в 1.
 def check_32_even_bit_set(value: int) -> bool:
-    return ...
+    check_mask = 0b10101010101010101010101010101010
+    return bool(value & check_mask)
 
 
 # Посчитать количество бит в 32-х битном числе, установленных в ноль.
 def calculate_32_zero_bits(value: int) -> int:
-    return ...
+    return bin(value)[2:].zfill(32).count("0")
 
 
 # Упаковать два целочисленных значения в 8 бит.
 # Первое число должно располагаться в 4 младших битах, второе число в - 4 старших.
 def pack_4_4(first: int, second: int) -> int:
-    return ...
+    if first > 0b1111 or second > 0b1111:
+        raise ValueError("Values must be between 0 and 15")
+    return second << 4 | first
 
 
 # Распоковать два целочисленных значения из 8 бит.
 # Первое число должно располагаться в 4 младших битах, второе число в - 4 старших.
-def unpack_4_4(first: int, second: int) -> int:
-    return ...
+def unpack_4_4(data) -> tuple[int, int]:
+    first = data & 0b00001111
+    second = (data & 0b11110000) >> 4
+    return first, second
 
 
 # Ограничить число заданным интервалом. Нижняя граница заданного интервала меньше либо равна верхней.
 def clamp(value: float, low: float, high: float) -> float:
-    return ...
+    return max(min(value, high), low)
 
 
 # Ограничить число заданным интервалом. Нижняя граница может быть как меньше, так и больше верхней.
 def clamp_any(value: float, low: float, high: float) -> float:
-    return ...
+    lower_bound = min(low, high)
+    upper_bound = max(low, high)
+    return max(min(value, upper_bound), lower_bound)
 
 
 # Вернуть True, если число нечетно и входит в интервал от -10 до 10.
 def event_and_match_interval_m10_10(value: int) -> bool:
-    return ...
+    return value % 2 != 0 and -10 <= value <= 10
 
 
 # Определить порядок выолнения операций и расстваить скобки таким образом, чтобы он стал обратным.
 def reverse_operations(value: float):
-    return value ** 4 * 0.5 + 0.25
+    return value ** (4 * (0.5 + 0.25))
 
 
 # Установить n-ый бит числа в единицу.
 def set_nth_bit(value: int, n: int) -> int:
-    return ...
+    return value | (1 << n)
 
 
 # Переключить n-ый бит числа.
 def switch_nth_bit(value: int, n: int) -> int:
-    return ...
+    return value ^ (1 << n)
 
 
 # Расставить скобки таким образом, чтобы выражение в задаче было возведено в степень 3.
 def change_priorities(x: float) -> float:
-    return x + 3 ** 3
+    return (x + 3) ** 3
 
 
 '''
